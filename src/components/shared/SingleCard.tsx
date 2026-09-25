5821a0a2b4a6d9827f0d2ef9c02319aae7f632af
sabrina01534 <sabrinasadia2006@gmail.com>
2026-09-25 09:25:13 +0600

    4th commit

--- src/components/shared/SingleCard.tsx
+++ src/components/shared/SingleCard.tsx
@@ -1,5 +1,6 @@
 import { Icard } from '@/types/cardtypes';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 
@@ -11,17 +12,26 @@ type IcardProps={
 const SingleCard = ({card}:IcardProps) => {
     return (
         <div className='group w-full overflow-hidden rounded-2xl border'>
-           <div className="card bg-base-100 w-96 shadow-sm">
+           <div className="card bg-base-100 shadow-sm">
   <figure>
-    <Image
+    <Image className='overflow-hidden object-cover h-60'
       src={card.image}
-      alt="card-img" />
+      alt={card.name} width={400} height={250}/>
   </figure>
   <div className="card-body">
+   <div className='flex gap-4'>
+    <button className="btn btn-xs bg-[#C2F800] font-bold text-black">{card.muscleGroups}</button>
+   <button className="btn btn-xs bg-[#C2F800] font-bold text-black">{card.equipment}</button>
+   
+   </div>
     <h2 className="card-title">{card.name}</h2>
-    <p>{card.description}</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+    <p className='mb-2'>{card.description}</p>
+    <div className=" border-t">
+    <div className='flex mt-2'>
+         <p className=''>{card.duration}</p>
+     <p>{card.caloriesBurned}</p>
+     <p>{card.rating}</p>
+    </div>
     </div>
   </div>
 </div> 
